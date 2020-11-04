8a24a0eaa6d52a7eb47ad6edbf48a8efd05d6909
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-04 17:03:51 -0600

    First commit. Starting to build JS, should have everything I need in HTML.

--- js-pixart-events/pixart.js
+++ js-pixart-events/pixart.js
@@ -1,1 +1,39 @@
+// Cached DOM Nodes
+const button = document.getElementById("set-color");
+const form = document.getElementById("form");
+const input = document.getElementById("color-field");
+const brush = document.getElementsByClassName("brush");
+const pixels = document.getElementsByClassName("square");
 
+// When you click the "Set Color" button , change the color of the
+// "brush" box to the value specfied by your input value
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let userInput = input.value;
+  brush.style.background = userInput;
+});
+
+// when user submits the form, we will save the value of the string
+// use this value to change the background color of the main square
+// also change the brush color
+
+// when you mouseover each div, have it change the background color of
+// that div to the color specified by your input value
+
+//
+
+//
+
+//
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
