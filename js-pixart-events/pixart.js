133036333038496fbacfaf48a6d323026071ab16
Tyler Samuelson <tylercsamuelson@gmail.com>
2020-11-04 22:01:33 -0600

    Final commit. Working Pixel Art app.

--- js-pixart-events/pixart.js
+++ js-pixart-events/pixart.js
@@ -1,40 +1,39 @@
 // Cached DOM Nodes
-const button = document.getElementById("set-color");
 const form = document.getElementById("form");
 const input = document.getElementById("color-field");
 const brush = document.getElementById("brush");
-const pixels = document.getElementsByClassName("square");
+const artboard = document.getElementById("artboard");
+const squares = document.getElementsByClassName("square");
+
+let brushColor = "";
 
 // When you click the "Set Color" button , change the color of the
 // "brush" box to the value specfied by your input value
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let userInput = input.value;
-  brush.style.backgroundColor = userInput;
+  brushColor = input.value;
+  brush.style.backgroundColor = brushColor;
   input.value = "";
 });
 
-// when user submits the form, we will save the value of the string
-// use this value to change the background color of the main square
-// also change the brush color
+// Create our ~8000 div blocks that will become our pixels/canvas!
+// Reconfigure to add the event listener when you make each pixel.
+for (let i = 0; i <= 8000; i++) {
+  let pixel = document.createElement("div");
+  pixel.classList.add("square");
+  artboard.appendChild(pixel);
+  pixel.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = brushColor;
+  });
+}
 
 // when you mouseover each div, have it change the background color of
 // that div to the color specified by your input value
-
-//
-
-//
-
-//
-
-//
-//
-//
-//
-//
-//
-//
-//
+// artboard.addEventListener("mouseover", (e) => {
+//   e.target.style.backgroundColor = brushColor;
+// });
+// //
+// //
 //
 //
 //
